ab32d0c937709fecb2e40e98b9146e6c6971da18
George Bentes <george.bentes@gmail.com>
2021-03-05 21:51:06 -0300

    Initial commit

---
+++ src/main/java/com/grglucastr/players/PlayersApplication.java
@@ -0,0 +1,13 @@
+package com.grglucastr.players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grglucastr/players/PlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grglucastr.players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
