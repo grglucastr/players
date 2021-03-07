b435ffd3c8de29ba84d4866ff1e4f8fd708b5a38
George Bentes <george.bentes@gmail.com>
2021-03-07 14:36:26 -0300

    Add repository

---
+++ src/main/java/com/grglucastr/players/repositories/ExpertRepository.java
@@ -0,0 +1,8 @@
+package com.grglucastr.players.repositories;
+
+import com.grglucastr.players.entities.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpertRepository extends JpaRepository<Expert, Integer> {
+
+}
