81d33a5aaa4431227d734eeedc06302d5865518f
George Bentes <george.bentes@gmail.com>
2021-03-07 17:01:47 -0300

    Add Kafka server address to config properties

--- src/main/java/com/grglucastr/players/producer/NoviceProducer.java
+++ src/main/java/com/grglucastr/players/producer/NoviceProducer.java
@@ -1,24 +1,44 @@
 package com.grglucastr.players.producer;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Properties;
 
 @Component
 public class NoviceProducer implements PlayerProducer {
 
-    private static final String BOOTSTRAP_SERVER = "127.0.0.7:9092";
     private static final String TOPIC_NAME = "novice-players";
+
+    @Value("${kafka.bootstrap-server.address}")
+    private String bootstrapServer;
+
+    @Value("${kafka.bootstrap-server.port}")
+    private String bootstrapServerPort;
 
     private Properties properties;
 
-    public NoviceProducer(){
+    @PostConstruct
+    public void init(){
+
+        if(StringUtils.isBlank(bootstrapServer)){
+            bootstrapServer = "127.0.0.1";
+        }
+
+        if(StringUtils.isBlank(bootstrapServerPort)){
+            bootstrapServer = "9092";
+        }
+
+        String server = bootstrapServer + ":" + bootstrapServerPort;
+
         this.properties = new Properties();
-        this.properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        this.properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         this.properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         this.properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     }
